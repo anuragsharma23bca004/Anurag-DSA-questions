7a1e7ba17a938f98a1018fe5839a6bfe7ae4e723
anuragsharma23bca004 <anuragsharma0690@gmail.com>
2024-09-01 23:40:15 +0530

    Update Linear search.c

--- Linear search.c
+++ Linear search.c
@@ -1,38 +1,19 @@
-#include <stdio.h>
-
-int main() {
-    int n, i, key, found = 0;
-
-    // Input: Number of elements in the array
-    printf("Enter the number of elements in the array: ");
-    scanf("%d", &n);
-
-    int arr[n];
-
-    // Input: Elements of the array
-    printf("Enter %d elements:\n", n);
-    for(i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    // Input: Element to search for
-    printf("Enter the element to search: ");
-    scanf("%d", &key);
-
-    // Linear search
-    for(i = 0; i < n; i++) {
-        if(arr[i] == key) {
-            found = 1;
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+    char chack = false;
+    char num;
+    printf("enter a number which are you seek:-");
+    scanf("%c",&num);
+    char arr[10]={'a','n.','u','r','a','g','w','x','y','z',};
+    for (int i=0;i<(sizeof(arr)/sizeof(char));i++){
+        if(arr[i]==num){
+            printf("Element is founded at index is:-%d",i);
+            chack=true;
             break;
         }
     }
-
-    // Output: Result of the search
-    if(found) {
-        printf("Element %d found at index %d.\n", key, i);
-    } else {
-        printf("Element %d not found in the array.\n", key);
+    if(chack==false){
+        printf("element is not founded in array ");
     }
-
-    return 0;
 }
